90787c9c0aa48750dbbd2cef90f2f2ed4211d2b0
Kulasingham P.N. <pasindunk2002@gmail.com>
2026-02-07 21:01:25 +0530

    Add age classification logic and print statements in numpy_v2.py

--- numpy_v2.py
+++ numpy_v2.py
@@ -3,3 +3,10 @@ import numpy as np
 ages = np.array([[25, 30, 35, 40, 45],
                  [50, 55, 60, 65, 70]])
 
+teens = ages[ages <= 19]
+adults = ages[(ages > 19) & (ages < 65)]
+elders = ages[ages >= 65]
+
+print("Teens:", teens)
+print("Adults:", adults)
+print("Elders:", elders)
