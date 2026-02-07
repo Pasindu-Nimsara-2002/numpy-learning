dcb8c6a607dc2490b46910b60880a872bb197f75
Kulasingham P.N. <pasindunk2002@gmail.com>
2026-02-07 21:08:09 +0530

    Add statistical calculations for the 2D array in numpy_functions.py

--- numpy_functions.py
+++ numpy_functions.py
@@ -5,3 +5,8 @@ array = np.array([[1, 2, 3, 4, 5],
                   [11, 12, 13, 14, 15]])
 
 print(np.shape(array))
+
+print(np.sum(array, axis=0))  # Sum along columns
+print(np.sum(array, axis=1))  # Sum along rows
+print(np.mean(array, axis=0))  # Mean along columns
+print(np.mean(array, axis=1))  # Mean along rows
